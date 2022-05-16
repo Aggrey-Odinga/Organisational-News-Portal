af65c419edfe35e7f7cc00c7cb75379447e0d44e
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-16 17:18:57 +0300

    Add getters and setters for department object

--- src/main/java/models/departments.java
+++ src/main/java/models/departments.java
@@ -6,4 +6,25 @@ public class departments {
     private int id;
     private int size;
 
+    public departments(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.size = 0;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getSize() { return size; }
+
+    public void setSize(int size) { this.size = size; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 }
