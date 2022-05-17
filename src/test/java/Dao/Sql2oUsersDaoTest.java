982aeeb30ef934ac9d3633d9729ef0ea8ec20a04
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-17 15:28:02 +0300

    Add tests for user implementation file

---
+++ src/test/java/Dao/Sql2oUsersDaoTest.java
@@ -0,0 +1,42 @@
+package Dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oUsersDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void addUserToDepartment() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void deleteByInt() {
+    }
+
+    @Test
+    public void clearAllUsers() {
+    }
+}
