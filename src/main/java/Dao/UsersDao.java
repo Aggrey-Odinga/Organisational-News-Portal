1c1da4c2420da2ade97a529c9f9812fccb4cdd47
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-17 14:28:53 +0300

    Add methods needed in interface file

--- src/main/java/Dao/UsersDao.java
+++ src/main/java/Dao/UsersDao.java
@@ -1,4 +1,25 @@
 package Dao;
 
+import models.Department;
+import models.Users;
+
+import java.util.List;
+
 public interface UsersDao {
+
+
+    List<Users> getAll();
+
+//    Create
+    void add(Users user);
+    void addUserToDepartment(Users user, Department department);
+
+//    Read
+    Users findById(int id);
+
+//    Delete
+    void deleteByInt(int id);
+    void clearAllUsers();
+
+
 }
