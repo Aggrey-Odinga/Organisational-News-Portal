e6cb2a37901619522da2a848bec08ad767e85e8b
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-17 16:28:38 +0300

    Add news Dao and its test file

--- src/main/java/Dao/Sql2oNewsDao.java
+++ src/main/java/Dao/Sql2oNewsDao.java
@@ -1,4 +1,63 @@
 package Dao;
 
-public class Sql2oNewsDao {
+import models.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao{
+
+    private final Sql2o sql2o;
+
+    public Sql2oNewsDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public List<News> getAll() {
+        try(Connection connection = sql2o.open()) {
+            return connection.createQuery("SELECT * FROM news").executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public void add(News news) {
+        String sql = "INSERT INTO news (name, author, description) VALUES (:name, :author, :description)";
+        try(Connection connection = sql2o.open()){
+            int id = (int) connection.createQuery(sql,true).bind(news).executeUpdate().getKey();
+            news.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public News findById(int id) {
+        try(Connection connection = sql2o.open()) {
+            return connection.createQuery("SELECT * FROM news WHERE id = :id").addParameter("id", id).executeAndFetchFirst(News.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from news WHERE id=:id";
+        try (Connection connection = sql2o.open()) {
+            connection.createQuery(sql).addParameter("id", id).executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from news";
+        try(Connection connection = sql2o.open()){
+            connection.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+
+    }
 }
---
+++ src/test/java/Dao/Sql2oNewsDaoTest.java
@@ -0,0 +1,34 @@
+package Dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oNewsDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void clearAll() {
+    }
+}
